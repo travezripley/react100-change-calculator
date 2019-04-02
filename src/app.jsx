411670bab4c816253f670f4d447e338a5fc60c49
Travezripley <theripleys@gmail.com>
2019-04-02 14:47:13 -0700

    updated app.jsx with monitary values on the UI

--- src/app.jsx
+++ src/app.jsx
@@ -73,10 +73,10 @@ class App extends Component {
       </div>
     </div>
 
-    {/*Right Panel-Output Area */ }
+    {/*Right Panel-Output Area*/ }
     <div className="col-md-8 results-wrapper">
       {/*Alerts, Success, Failure */}
-      <div className="panel -panel-default">
+      <div className="panel panel-default">
         <div className="panel-body">
           <div className="alert alert-success">
             {this.state.changeDue}
@@ -84,6 +84,95 @@ class App extends Component {
         </div>
       </div>
     </div>
+
+    {/*Output Panels-Top Row Start*/ }
+    <div className="row">
+      <div className="col-md-3">
+
+        {/*twenties*/}
+        <div className="panel panel-default">
+          <div className="well panel-footer">
+            <h4 className='text-center'>Twenties:</h4>
+            <p name="output-twenties" className='lead text-center'>{this.state.twenties}</p>
+          </div>
+        </div>
+      </div>
+
+      {/*tens*/}
+      <div className="col-md-3">
+        <div className="panel panel-default">
+          <div className="well panel-footer">
+            <h4 className='text-center'>Tens:</h4>
+            <p name="output-tens" className='lead text-center'>{this.state.tens}</p>
+          </div>
+        </div>
+      </div>
+
+      {/*fives*/}
+      <div className="col-md-3">
+        <div className="panel panel-default">
+          <div className="well panel-footer">
+            <h4 className='text-center'>Fives:</h4>
+            <p name="output-fives" className='lead text-center'>{this.state.fives}</p>
+          </div>
+        </div>
+      </div>
+
+      {/*ones*/}
+      <div className="col-md-3">
+        <div className="panel panel-default">
+          <div className="well panel-footer">
+            <h4 className='text-center'>Ones:</h4>
+            <p name="output-ones" className='lead text-center'>{this.state.ones}</p>
+          </div>
+        </div>
+      </div>
+
+{/*Bottom Row - Start*/}
+
+ {/*quarters*/}
+ <div className="col-md-3">
+        <div className="panel panel-default">
+          <div className="well panel-footer">
+            <h4 className='text-center'>Quarters:</h4>
+            <p name="output-quarters" className='lead text-center'>{this.state.quarters}</p>
+          </div>
+        </div>
+      </div>
+
+ {/*dimes*/}
+ <div className="col-md-3">
+        <div className="panel panel-default">
+          <div className="well panel-footer">
+            <h4 className='text-center'>Dimes:</h4>
+            <p name="output-dimes" className='lead text-center'>{this.state.dimes}</p>
+          </div>
+        </div>
+      </div>
+
+ {/*nickles*/}
+ <div className="col-md-3">
+        <div className="panel panel-default">
+          <div className="well panel-footer">
+            <h4 className='text-center'>Nickles:</h4>
+            <p name="output-nickles" className='lead text-center'>{this.state.nickles}</p>
+          </div>
+        </div>
+      </div>
+
+       {/*pennies*/}
+ <div className="col-md-3">
+        <div className="panel panel-default">
+          <div className="well panel-footer">
+            <h4 className='text-center'>Pennies:</h4>
+            <p name="output-pennies" className='lead text-center'>{this.state.pennies}</p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+          
+
   }
 }
 
