a147470c91ebc513aa5e83563ab698854eac477b
Travez Ripley <theripleys@gmail.com>
2019-04-22 18:25:07 -0700

    updates

--- src/app.jsx
+++ src/app.jsx
@@ -3,13 +3,13 @@ import React, { Component } from 'react';
 class App extends Component {
   constructor(props) {
     super(props);
-    // setting initial state below
+        // setting initial state below
     this.state = {
       amountDue: '',
       amountReceived: '',
       changeDue: 'Enter amount due and amount received on the left',
       twenties: '0',
-      tens: '0', 
+      tens: '0',
       fives: '0',
       ones: '0',
       quarters: '0',
@@ -17,11 +17,9 @@ class App extends Component {
       nickels: '0',
       pennies: '0'
     };
-    // event binding for updating state values
+        // event binding for updating state values
     this.updateAmountDue = this.updateAmountDue.bind(this);
     this.updateAmountReceived = this.updateAmountReceived.bind(this);
-
-    // event binding for making calculation
     this.handleClick = this.handleClick.bind(this);
     this.calculate = this.calculate.bind(this);
   }
@@ -34,56 +32,55 @@ class App extends Component {
 
   updateAmountReceived(e) {
     this.setState({
-      amountReceived: e.target.value 
+      amountReceived: e.target.value
     });
   }
 
   calculate(amountDue, amountReceived) {
-     //dealing with dollars below
-    const changeDue = (amountReceived - amountDue).toFixed(2);
-    const dollarsTotal = Math.trunc(changeDue);
-    //individual bill calculations
-    const twenties = Math.floor(dollarsTotal/20);
-    const tens = Math.floor((dollarsTotal%20)/10);
-    const fives = Math.floor(((dollarsTotal%20)%10)/5);
-    const ones = Math.floor((dollarsTotal%20)%10)%5;
-     //dealing with cents below
-    const centsConverted = Math.round(changeDue * 100);
-    const cents = Number((centsConverted.toString().slice(-2)));
-    //individual coin calculations
-    const quarters = Math.floor(cents/25); 
-    const dimes = Math.floor((cents%25)/10); 
-    const nickels = Math.floor(((cents%25)%10)/5);         
-    const pennies = Math.floor((cents%25)%10)%5;
-    
-      //console.log(twenties, tens, fives, ones, quarters, dimes, nickels, pennies);
+        // dealing with dollars below
+    let changeDue = (amountReceived - amountDue).toFixed(2);
+    let dollarsTotal = Math.trunc(changeDue);
+        // individual bill calculations
+    let twenties = Math.floor(dollarsTotal / 20);
+    let tens = Math.floor((dollarsTotal % 20) / 10);
+    let fives = Math.floor(((dollarsTotal % 20) % 10) / 5);
+    let ones = Math.floor((dollarsTotal % 20) % 10) % 5;
+        // dealing with cents below
+    let centsConverted = Math.round(changeDue * 100);
+    let cents = Number(centsConverted.toString().slice(-2));
+        // individual coin calculations
+    let quarters = Math.floor(cents / 25);
+    let dimes = Math.floor((cents % 25) / 10);
+    let nickels = Math.floor(((cents % 25) % 10) / 5);
+    let pennies = Math.floor((cents % 25) % 10) % 5;
+
+        // console.log(twenties, tens, fives, ones, quarters, dimes, nickels, pennies);
 
     this.setState({
-      changeDue: 'The total change due is $' + changeDue,
-      twenties,
-      tens,
-      fives,
-      ones,
-      quarters,
-      dimes,
-      nickels,
-      pennies
+      changeDue: `The total change due is $${changeDue}`,
+      twenties: twenties,
+      tens: tens,
+      fives: fives,
+      ones: ones,
+      quarters: quarters,
+      dimes: dimes,
+      nickels: nickels,
+      pennies: pennies
     });
   }
 
   handleClick(e) {
     e.preventDefault();
-    
+
     const amountDue = this.state.amountDue;
     const amountReceived = this.state.amountReceived;
-    
+
     this.calculate(amountDue, amountReceived);
   }
 
   render() {
     return (
       <div className='container'>
-        
         <div className='page-header'>
           <h1 className='text-white'>Change Calculator</h1>
         </div>
@@ -94,16 +91,32 @@ class App extends Component {
               <div className='panel-heading'>Enter Information</div>
               <div className='panel-body'>
                 <div className='form-group'>
-                  <label for='amountDue'>How much is due?</label>
-                    <input name='amountDue' onChange={this.updateAmountDue} value={this.state.amountDue} className='form-control input-md' />
+                  <label htmlFor='amountDue'>How much is due?</label>
+                  <input
+                    name='amountDue'
+                    onChange={ this.updateAmountDue }
+                    value={ this.state.amountDue }
+                    className='form-control input-md'
+                  />
                 </div>
                 <div className='form-group'>
-                  <label for='amountReceived'>How much was received?</label>  
-                  <input name='amountReceived' onChange={this.updateAmountReceived} value={this.state.amountReceived} className='form-control input-md' />
-                </div>  
+                  <label htmlFor='amountReceived'>How much was received?</label>
+                  <input
+                    name='amountReceived'
+                    onChange={ this.updateAmountReceived }
+                    value={ this.state.amountReceived }
+                    className='form-control input-md'
+                  />
+                </div>
                 <div className='panel-footer'>
-                  <button name='button' onClick={this.handleClick} className='btn btn-primary form-control'>Calculate</button>
-                </div>  
+                  <button
+                    name='button'
+                    onClick={ this.handleClick }
+                    className='btn btn-primary form-control'
+                  >
+                                        Calculate
+                                    </button>
+                </div>
               </div>
             </div>
           </div>
@@ -112,88 +125,113 @@ class App extends Component {
             {/* alerts, success and failure */}
             <div className='panel panel-default'>
               <div className='panel-body'>
-                <div className='alert alert-success'>
-                  {this.state.changeDue}
-                </div>
+                <div className='alert alert-success'>{this.state.changeDue}</div>
                 {/* <div className='invisible alert alert-danger'>
                   <strong>Danger:</strong> Additional money owed
                 </div> */}
-              {/* output panels, top row begin */}
-              <div className='row'>
-                <div className='col-md-3'>
-                  <div className='panel panel-default'>
-                    <div className='well panel-footer'>
-                      <h4 className='text-center'>Twenties</h4>
-                      <p name='output-twenties' className='lead text-center'>{this.state.twenties}</p>
-                    </div>
-                  </div>  
+                {/* output panels, top row begin */}
+                <div className='row'>
+                  <div className='col-md-3'>
+                    <div className='panel panel-default'>
+                          <div className='well panel-footer'>
+                              <h4 className='text-center'>Twenties</h4>
+                              <p
+                                  name='output-twenties'
+                                  className='change lead text-center'
+                                >
+                                  {this.state.twenties}
+                                </p>
+                            </div>
+                        </div>
+                  </div>
+                  <div className='col-md-3'>
+                    <div className='panel panel-default'>
+                          <div className='well panel-footer'>
+                              <h4 className='text-center'>Tens</h4>
+                              <p name='output-tens' className=' change lead text-center'>
+                                  {this.state.tens}
+                                </p>
+                            </div>
+                        </div>
+                  </div>
+                  <div className='col-md-3'>
+                    <div className='panel panel-default'>
+                          <div className='well panel-footer'>
+                              <h4 className='text-center'>Fives</h4>
+                              <p name='output-fives' className=' change lead text-center'>
+                                  {this.state.fives}
+                                </p>
+                            </div>
+                        </div>
+                  </div>
+                  <div className='col-md-3'>
+                    <div className='panel panel-default'>
+                          <div className='well panel-footer'>
+                              <h4 className='text-center'>Ones</h4>
+                              <p name='output-ones' className='change lead text-center'>
+                                  {this.state.ones}
+                                </p>
+                            </div>
+                        </div>
+                  </div>
                 </div>
-                <div className='col-md-3'>
-                  <div className='panel panel-default'>
-                    <div className='well panel-footer'>
-                      <h4 className='text-center'>Tens</h4>
-                      <p name='output-tens' className='lead text-center'>{this.state.tens}</p>
-                    </div>
-                  </div>  
-                </div>
-                <div className='col-md-3'>
-                  <div className='panel panel-default'>
-                    <div className='well panel-footer'>
-                      <h4 className='text-center'>Fives</h4>
-                      <p name='output-fives' className='lead text-center'>{this.state.fives}</p>
-                    </div>
-                  </div>  
-                </div>
-                <div className='col-md-3'>
-                  <div className='panel panel-default'>
-                    <div className='well panel-footer'>
-                      <h4 className='text-center'>Ones</h4>
-                      <p name='output-ones' className='lead text-center'>{this.state.ones}</p>
-                    </div>
-                  </div>  
+                {/* bottom row begin */}
+                <div className='row'>
+                  <div className='col-md-3'>
+                    <div className='panel panel-default'>
+                          <div className='well panel-footer'>
+                              <h4 className='text-center'>Quarters</h4>
+                              <p
+                                  name='output-quarters'
+                                  className='change lead text-center'
+                                >
+                                  {this.state.quarters}
+                                </p>
+                            </div>
+                        </div>
+                  </div>
+                  <div className='col-md-3'>
+                    <div className='panel panel-default'>
+                          <div className='well panel-footer'>
+                              <h4 className='text-center'>Dimes</h4>
+                              <p name='output-dimes' className=' change lead text-center'>
+                                  {this.state.dimes}
+                                </p>
+                            </div>
+                        </div>
+                  </div>
+                  <div className='col-md-3'>
+                    <div className='panel panel-default'>
+                          <div className='well panel-footer'>
+                              <h4 className='text-center'>Nickels</h4>
+                              <p
+                                  name='output-nickels'
+                                  className='change lead text-center'
+                                >
+                                  {this.state.nickels}
+                                </p>
+                            </div>
+                        </div>
+                  </div>
+                  <div className='col-md-3'>
+                    <div className='panel panel-default'>
+                          <div className='well panel-footer'>
+                              <h4 className='text-center'>Pennies</h4>
+                              <p
+                                  name='output-pennies'
+                                  className='change lead text-center'
+                                >
+                                  {this.state.pennies}
+                                </p>
+                            </div>
+                        </div>
+                  </div>
                 </div>
               </div>
-              {/* bottom row begin */}
-              <div className='row'>
-                <div className='col-md-3'>
-                  <div className='panel panel-default'>
-                    <div className='well panel-footer'>
-                      <h4 className='text-center'>Quarters</h4>
-                      <p name='output-quarters' className='lead text-center'>{this.state.quarters}</p>
-                    </div>
-                  </div>  
-                </div>
-                <div className='col-md-3'>
-                  <div className='panel panel-default'>
-                    <div className='well panel-footer'>
-                      <h4 className='text-center'>Dimes</h4>
-                      <p name='output-dimes' className='lead text-center'>{this.state.dimes}</p>
-                    </div>
-                  </div>  
-                </div>
-                <div className='col-md-3'>
-                  <div className='panel panel-default'>
-                    <div className='well panel-footer'>
-                      <h4 className='text-center'>Nickels</h4>
-                      <p name='output-nickels' className='lead text-center'>{this.state.nickels}</p>
-                    </div>
-                  </div>  
-                </div>
-                <div className='col-md-3'>
-                  <div className='panel panel-default'>
-                    <div className='well panel-footer'>
-                      <h4 className='text-center'>Pennies</h4>
-                      <p name='output-pennies' className='lead text-center'>{this.state.pennies}</p>
-                    </div>
-                  </div>  
-                </div>
-              </div>
-              </div>  
             </div>
           </div>
         </div>
-        
-      </div>  
+      </div>
     );
   }
 }
